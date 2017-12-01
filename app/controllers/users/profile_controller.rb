f953b98380da8de1a4bcc217da63c5877fcf5b61
Kalan Chen <kjj6198@gmail.com>
2017-12-01 15:22:10 +0800

    add personal history

--- app/controllers/users/profile_controller.rb
+++ app/controllers/users/profile_controller.rb
@@ -1,5 +1,8 @@
 class Users::ProfileController < ApplicationController
+  before_action :auth_google_user!
   def profile
-
+    @histories = current_user.orders.order(created_at: :desc).limit(1000)
+    @histories_sum = current_user.orders.sum(:price)
+    @histories_count = current_user.orders.count(:id)
   end
 end
